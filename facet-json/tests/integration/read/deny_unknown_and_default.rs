9593e82474391af217059352e0942d19156a6fd7
Amos Wenger <amos@bearcove.eu>
2025-04-20 19:05:44 +0200

    fewer asserts

--- facet-json/tests/integration/read/deny_unknown_and_default.rs
+++ facet-json/tests/integration/read/deny_unknown_and_default.rs
@@ -14,9 +14,7 @@ fn test_struct_with_missing_field() -> eyre::Result<()> {
 
     let json_data = r#"{"foo": "example", "bar": 100}"#;
     let result: Result<ThreeField, _> = from_str(json_data);
-    let err = result
-        .err()
-        .ok_or_else(|| eyre::eyre!("Expected an error, but deserialization succeeded"))?;
+    let err = result.expect_err("Expected an error, but deserialization succeeded");
     insta::assert_debug_snapshot!(err);
     Ok(())
 }
@@ -34,16 +32,13 @@ fn test_deny_unknown_fields() -> eyre::Result<()> {
 
     // JSON with only expected fields
     let json_ok = r#"{"foo":"abc","bar":42}"#;
-    let result_ok: Result<StrictStruct, _> = from_str(json_ok);
-    let _strict: StrictStruct =
-        result_ok.map_err(|e| eyre::eyre!("Expected Ok for json_ok, got error: {}", e))?;
+    let _strict: StrictStruct = from_str(json_ok)?;
 
     // JSON with an unexpected extra field should generate an error
     let json_extra = r#"{"foo":"abc","bar":42,"baz":true}"#;
     let result_extra: Result<StrictStruct, _> = from_str(json_extra);
-    let err = result_extra.err().ok_or_else(|| {
-        eyre::eyre!("Expected error for json_extra, but deserialization succeeded")
-    })?;
+    let err =
+        result_extra.expect_err("Expected error for json_extra, but deserialization succeeded");
     insta::assert_debug_snapshot!(err);
     Ok(())
 }
@@ -63,17 +58,13 @@ fn json_read_struct_level_default_unset_field() -> eyre::Result<()> {
     // Only set foo, leave bar missing - should use Default for String
     let json = r#"{"foo": 123}"#;
 
-    let s: DefaultStruct =
-        from_str(json).map_err(|e| eyre::eyre!("Error deserializing JSON: {}", e))?;
-    if s.foo != 123 {
-        return Err(eyre::eyre!("Expected foo to be 123, got {}", s.foo));
-    }
-    if s.bar != "" {
-        return Err(eyre::eyre!(
-            "Expected bar to be empty string, got {:?}",
-            s.bar
-        ));
-    }
+    let s: DefaultStruct = from_str(json)?;
+    assert_eq!(s.foo, 123, "Expected foo to be 123, got {}", s.foo);
+    assert!(
+        s.bar.is_empty(),
+        "Expected bar to be empty string, got {:?}",
+        s.bar
+    );
     Ok(())
 }
 
@@ -92,17 +83,13 @@ fn json_read_field_level_default_no_function() -> eyre::Result<()> {
     // Only set foo, leave bar missing - should use Default for String
     let json = r#"{"foo": 789}"#;
 
-    let s: FieldDefault =
-        from_str(json).map_err(|e| eyre::eyre!("Error deserializing JSON: {}", e))?;
-    if s.foo != 789 {
-        return Err(eyre::eyre!("Expected foo to be 789, got {}", s.foo));
-    }
-    if s.bar != "" {
-        return Err(eyre::eyre!(
-            "Expected bar to be empty string, got {:?}",
-            s.bar
-        ));
-    }
+    let s: FieldDefault = from_str(json)?;
+    assert_eq!(s.foo, 789, "Expected foo to be 789, got {}", s.foo);
+    assert_eq!(
+        s.bar, "",
+        "Expected bar to be empty string, got {:?}",
+        s.bar
+    );
     Ok(())
 }
 
@@ -125,14 +112,9 @@ fn json_read_field_level_default_function() -> eyre::Result<()> {
     // Only set bar, leave foo missing - should use default_number()
     let json = r#"{"bar": "hello"}"#;
 
-    let s: FieldDefaultFn =
-        from_str(json).map_err(|e| eyre::eyre!("Error deserializing JSON: {}", e))?;
-    if s.foo != 12345 {
-        return Err(eyre::eyre!("Expected foo to be 12345, got {}", s.foo));
-    }
-    if s.bar != "hello" {
-        return Err(eyre::eyre!("Expected bar to be 'hello', got {}", s.bar));
-    }
+    let s: FieldDefaultFn = from_str(json)?;
+    assert_eq!(s.foo, 12345, "Expected foo to be 12345, got {}", s.foo);
+    assert_eq!(s.bar, "hello", "Expected bar to be 'hello', got {}", s.bar);
     Ok(())
 }
 
@@ -148,13 +130,11 @@ fn test_allow_unknown_fields_1() -> eyre::Result<()> {
 
     // JSON with only expected fields
     let json_ok = r#"{"foo":"abc","bar":42}"#;
-    let _ = from_str::<PermissiveStruct>(json_ok)
-        .map_err(|e| eyre::eyre!("Error deserializing json_ok: {}", e))?;
+    let _ = from_str::<PermissiveStruct>(json_ok)?;
 
     // JSON with an unexpected extra field should NOT generate an error
     let json_extra = r#"{"foo":"abc","bar":42,"baz":[]}"#;
-    let _ = from_str::<PermissiveStruct>(json_extra)
-        .map_err(|e| eyre::eyre!("Error deserializing json_extra: {}", e))?;
+    let _ = from_str::<PermissiveStruct>(json_extra)?;
     Ok(())
 }
 
@@ -189,13 +169,12 @@ fn test_allow_unknown_fields_complex() -> eyre::Result<()> {
         ]
     }
     "#;
-    let result: PermissiveStruct = from_str(json_complex)
-        .map_err(|e| eyre::eyre!("Error deserializing json_complex: {}", e))?;
-    if result.foo != "xyz" {
-        return Err(eyre::eyre!("Expected foo to be 'xyz', got {}", result.foo));
-    }
-    if result.bar != 99 {
-        return Err(eyre::eyre!("Expected bar to be 99, got {}", result.bar));
-    }
+    let result: PermissiveStruct = from_str(json_complex)?;
+    assert_eq!(
+        result.foo, "xyz",
+        "Expected foo to be 'xyz', got {}",
+        result.foo
+    );
+    assert_eq!(result.bar, 99, "Expected bar to be 99, got {}", result.bar);
     Ok(())
 }
