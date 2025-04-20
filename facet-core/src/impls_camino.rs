a969a0fc3613abbf05ab513d0177707c76f285f4
Amos Wenger <amos@bearcove.eu>
2025-04-20 06:21:31 +0200

    Let Utf8PathBuf implement Parse

--- facet-core/src/impls_camino.rs
+++ facet-core/src/impls_camino.rs
@@ -2,7 +2,9 @@ use core::alloc::Layout;
 
 use camino::{Utf8Path, Utf8PathBuf};
 
-use crate::{ConstTypeId, Def, Facet, ScalarAffinity, ScalarDef, Shape, value_vtable};
+use crate::{
+    ConstTypeId, Def, Facet, ScalarAffinity, ScalarDef, Shape, value_vtable, value_vtable_inner,
+};
 
 unsafe impl Facet for Utf8PathBuf {
     const SHAPE: &'static Shape = &const {
@@ -14,7 +16,14 @@ unsafe impl Facet for Utf8PathBuf {
                     .affinity(ScalarAffinity::path().build())
                     .build(),
             ))
-            .vtable(value_vtable!((), |f, _opts| write!(f, "Utf8PathBuf")))
+            .vtable(
+                &const {
+                    let mut vtable = value_vtable_inner!((), |f, _opts| write!(f, "Utf8PathBuf"));
+                    vtable.parse =
+                        Some(|s, target| Ok(unsafe { target.put(Utf8Path::new(s).to_owned()) }));
+                    vtable
+                },
+            )
             .build()
     };
 }
