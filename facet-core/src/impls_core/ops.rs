23918dc4b34fb3c6f1296b46ed68333f620b2138
Amos Wenger <amos@bearcove.eu>
2025-05-09 18:27:54 +0200

    Fix ops::Range implementation

--- facet-core/src/impls_core/ops.rs
+++ facet-core/src/impls_core/ops.rs
@@ -1,6 +1,4 @@
-use crate::{
-    ConstTypeId, Def, Facet, Field, Shape, StructDef, StructKind, VTableView, ValueVTable,
-};
+use crate::{ConstTypeId, Facet, Field, Shape, StructType, Type, VTableView, ValueVTable};
 use core::{alloc::Layout, mem};
 
 unsafe impl<'a, Idx: Facet<'a>> Facet<'a> for core::ops::Range<Idx> {
@@ -12,53 +10,51 @@ unsafe impl<'a, Idx: Facet<'a>> Facet<'a> for core::ops::Range<Idx> {
             }])
             .id(ConstTypeId::of::<Self>())
             .layout(Layout::new::<Self>())
-            .vtable(
-                &const {
-                    let mut builder = ValueVTable::builder::<Self>().type_name(|f, opts| {
-                        write!(f, "Range")?;
-                        if let Some(opts) = opts.for_children() {
-                            write!(f, "<")?;
-                            (Idx::SHAPE.vtable.type_name)(f, opts)?;
-                            write!(f, ">")?;
-                        } else {
-                            write!(f, "<…>")?;
-                        }
-                        Ok(())
-                    });
-
-                    if Idx::SHAPE.is_debug() {
-                        builder = builder.debug(|this, f| {
-                            (<VTableView<Idx>>::of().debug().unwrap())(&this.start, f)?;
-                            write!(f, "..")?;
-                            (<VTableView<Idx>>::of().debug().unwrap())(&this.end, f)?;
-                            Ok(())
-                        });
-                    }
-
-                    builder.build()
-                },
-            )
-            .def(Def::Struct(
-                StructDef::builder()
-                    .kind(StructKind::Struct)
+            .ty(Type::User(crate::UserType::Struct(
+                StructType::builder()
                     .fields(
                         &const {
                             [
                                 Field::builder()
                                     .name("start")
-                                    .shape(|| Idx::SHAPE)
+                                    .shape(Idx::SHAPE)
                                     .offset(mem::offset_of!(core::ops::Range<Idx>, start))
                                     .build(),
                                 Field::builder()
                                     .name("end")
-                                    .shape(|| Idx::SHAPE)
+                                    .shape(Idx::SHAPE)
                                     .offset(mem::offset_of!(core::ops::Range<Idx>, start))
                                     .build(),
                             ]
                         },
                     )
                     .build(),
-            ))
+            )))
             .build()
+    };
+
+    const VTABLE: &'static ValueVTable = &const {
+        let mut builder = ValueVTable::builder::<Self>().type_name(|f, opts| {
+            write!(f, "Range")?;
+            if let Some(opts) = opts.for_children() {
+                write!(f, "<")?;
+                (Idx::SHAPE.vtable.type_name)(f, opts)?;
+                write!(f, ">")?;
+            } else {
+                write!(f, "<…>")?;
+            }
+            Ok(())
+        });
+
+        if Idx::SHAPE.is_debug() {
+            builder = builder.debug(|this, f| {
+                (<VTableView<Idx>>::of().debug().unwrap())(&this.start, f)?;
+                write!(f, "..")?;
+                (<VTableView<Idx>>::of().debug().unwrap())(&this.end, f)?;
+                Ok(())
+            });
+        }
+
+        builder.build()
     };
 }
