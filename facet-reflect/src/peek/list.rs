f5be02a8598c9f8aaf48228fce3c4e6ddbe1f466
Kyle Lacy <kylelacy@kyle.space>
2025-05-17 01:57:11 -0700

    Update `PeekList` to support lists without `as_ptr` impl

--- facet-reflect/src/peek/list.rs
+++ facet-reflect/src/peek/list.rs
@@ -1,24 +1,38 @@
 use super::Peek;
-use core::fmt::Debug;
-use facet_core::ListDef;
+use core::{fmt::Debug, marker::PhantomData};
+use facet_core::{ListDef, PtrConst, PtrMut};
 
 /// Iterator over a `PeekList`
 pub struct PeekListIter<'mem, 'facet_lifetime> {
-    list: PeekList<'mem, 'facet_lifetime>,
+    state: PeekListIterState<'mem>,
     index: usize,
     len: usize,
+    def: ListDef,
+    _list: PhantomData<Peek<'mem, 'facet_lifetime>>,
 }
 
 impl<'mem, 'facet_lifetime> Iterator for PeekListIter<'mem, 'facet_lifetime> {
     type Item = Peek<'mem, 'facet_lifetime>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.len {
-            return None;
-        }
-        let item = self.list.get(self.index);
+        let item_ptr = match self.state {
+            PeekListIterState::Ptr { data, stride } => {
+                if self.index >= self.len {
+                    return None;
+                }
+
+                unsafe { data.field(stride * self.index) }
+            }
+            PeekListIterState::Iter { iter } => unsafe {
+                (self.def.vtable.iter_vtable.next)(iter)?
+            },
+        };
+
+        // Update the index. This is used pointer iteration and for
+        // calculating the iterator's size
         self.index += 1;
-        item
+
+        Some(unsafe { Peek::unchecked_new(item_ptr, self.def.t()) })
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -36,6 +50,11 @@ impl<'mem, 'facet_lifetime> IntoIterator for &'mem PeekList<'mem, 'facet_lifetim
     fn into_iter(self) -> Self::IntoIter {
         self.iter()
     }
+}
+
+enum PeekListIterState<'mem> {
+    Ptr { data: PtrConst<'mem>, stride: usize },
+    Iter { iter: PtrMut<'mem> },
 }
 
 /// Lets you read from a list (implements read-only [`facet_core::ListVTable`] proxies)
@@ -66,30 +85,40 @@ impl<'mem, 'facet_lifetime> PeekList<'mem, 'facet_lifetime> {
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
+
     /// Get an item from the list at the specified index
     pub fn get(&self, index: usize) -> Option<Peek<'mem, 'facet_lifetime>> {
-        if index >= self.len() {
-            return None;
-        }
+        let item = unsafe { (self.def.vtable.get)(self.value.data(), index)? };
 
-        let Ok(layout) = self.def.t().layout.sized_layout() else {
-            return None;
-        };
-
-        let data = unsafe { (self.def.vtable.as_ptr.unwrap())(self.value.data()) };
-
-        // SAFETY: we verify index bounds at the start of the function
-        let item_ptr = unsafe { data.field(layout.size() * index) };
-
-        Some(unsafe { Peek::unchecked_new(item_ptr, self.def.t()) })
+        Some(unsafe { Peek::unchecked_new(item, self.def.t()) })
     }
 
     /// Returns an iterator over the list
     pub fn iter(self) -> PeekListIter<'mem, 'facet_lifetime> {
+        let state = if let Some(as_ptr_fn) = self.def.vtable.as_ptr {
+            let data = unsafe { as_ptr_fn(self.value.data()) };
+            let layout = self
+                .def
+                .t()
+                .layout
+                .sized_layout()
+                .expect("can only iterate over sized list elements");
+            let stride = layout.size();
+
+            PeekListIterState::Ptr { data, stride }
+        } else {
+            let iter = unsafe {
+                (self.def.vtable.iter_vtable.init_with_value.unwrap())(self.value.data())
+            };
+            PeekListIterState::Iter { iter }
+        };
+
         PeekListIter {
-            list: self,
+            state,
             index: 0,
             len: self.len(),
+            def: self.def(),
+            _list: PhantomData,
         }
     }
 
