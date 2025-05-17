371bfdb7bb7edef2b09c0bd134407f12a98f1143
Kyle Lacy <kylelacy@kyle.space>
2025-05-17 02:33:22 -0700

    Update `PeekListLike` to support lists without `as_ptr` impl

--- facet-reflect/src/peek/list_like.rs
+++ facet-reflect/src/peek/list_like.rs
@@ -1,7 +1,7 @@
-use facet_core::{PtrConst, Shape, ShapeLayout};
+use facet_core::{IterVTable, PtrConst, PtrMut, Shape, ShapeLayout};
 
 use super::Peek;
-use core::fmt::Debug;
+use core::{fmt::Debug, marker::PhantomData};
 
 /// Fields for types which act like lists
 #[derive(Clone, Copy)]
@@ -35,21 +35,33 @@ impl ListLikeDef {
 
 /// Iterator over a `PeekListLike`
 pub struct PeekListLikeIter<'mem, 'facet_lifetime> {
-    list: PeekListLike<'mem, 'facet_lifetime>,
+    state: PeekListLikeIterState<'mem>,
     index: usize,
     len: usize,
+    def: ListLikeDef,
+    _list: PhantomData<Peek<'mem, 'facet_lifetime>>,
 }
 
 impl<'mem, 'facet_lifetime> Iterator for PeekListLikeIter<'mem, 'facet_lifetime> {
     type Item = Peek<'mem, 'facet_lifetime>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.len {
-            return None;
-        }
-        let item = self.list.get(self.index);
+        let item_ptr = match self.state {
+            PeekListLikeIterState::Ptr { data, stride } => {
+                if self.index >= self.len {
+                    return None;
+                }
+
+                unsafe { data.field(stride * self.index) }
+            }
+            PeekListLikeIterState::Iter { iter, vtable } => unsafe { (vtable.next)(iter)? },
+        };
+
+        // Update the index. This is used pointer iteration and for
+        // calculating the iterator's size
         self.index += 1;
-        item
+
+        Some(unsafe { Peek::unchecked_new(item_ptr, self.def.t()) })
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -69,13 +81,23 @@ impl<'mem, 'facet_lifetime> IntoIterator for &'mem PeekListLike<'mem, 'facet_lif
     }
 }
 
+enum PeekListLikeIterState<'mem> {
+    Ptr {
+        data: PtrConst<'mem>,
+        stride: usize,
+    },
+    Iter {
+        iter: PtrMut<'mem>,
+        vtable: IterVTable<PtrConst<'static>>,
+    },
+}
+
 /// Lets you read from a list, array or slice
 #[derive(Clone, Copy)]
 pub struct PeekListLike<'mem, 'facet_lifetime> {
     pub(crate) value: Peek<'mem, 'facet_lifetime>,
     pub(crate) def: ListLikeDef,
     len: usize,
-    as_ptr: unsafe fn(this: PtrConst) -> PtrConst,
 }
 
 impl Debug for PeekListLike<'_, '_> {
@@ -87,20 +109,12 @@ impl Debug for PeekListLike<'_, '_> {
 impl<'mem, 'facet_lifetime> PeekListLike<'mem, 'facet_lifetime> {
     /// Creates a new peek list
     pub fn new(value: Peek<'mem, 'facet_lifetime>, def: ListLikeDef) -> Self {
-        let (len, as_ptr_fn) = match def {
-            ListLikeDef::List(v) => (
-                unsafe { (v.vtable.len)(value.data()) },
-                v.vtable.as_ptr.unwrap(),
-            ),
-            ListLikeDef::Slice(v) => (unsafe { (v.vtable.len)(value.data()) }, v.vtable.as_ptr),
-            ListLikeDef::Array(v) => (v.n, v.vtable.as_ptr),
+        let len = match def {
+            ListLikeDef::List(v) => unsafe { (v.vtable.len)(value.data()) },
+            ListLikeDef::Slice(v) => unsafe { (v.vtable.len)(value.data()) },
+            ListLikeDef::Array(v) => v.n,
         };
-        Self {
-            value,
-            def,
-            len,
-            as_ptr: as_ptr_fn,
-        }
+        Self { value, def, len }
     }
 
     /// Get the length of the list
@@ -119,12 +133,22 @@ impl<'mem, 'facet_lifetime> PeekListLike<'mem, 'facet_lifetime> {
     ///
     /// Panics if the index is out of bounds
     pub fn get(&self, index: usize) -> Option<Peek<'mem, 'facet_lifetime>> {
+        let as_ptr = match self.def {
+            ListLikeDef::List(def) => {
+                // Call get from the list's vtable directly if available
+                let item = unsafe { (def.vtable.get)(self.value.data(), index)? };
+                return Some(unsafe { Peek::unchecked_new(item, self.def.t()) });
+            }
+            ListLikeDef::Array(def) => def.vtable.as_ptr,
+            ListLikeDef::Slice(def) => def.vtable.as_ptr,
+        };
+
         if index >= self.len() {
             return None;
         }
 
         // Get the base pointer of the array
-        let base_ptr = unsafe { (self.as_ptr)(self.value.data()) };
+        let base_ptr = unsafe { as_ptr(self.value.data()) };
 
         // Get the layout of the element type
         let elem_layout = match self.def.t().layout {
@@ -143,10 +167,38 @@ impl<'mem, 'facet_lifetime> PeekListLike<'mem, 'facet_lifetime> {
 
     /// Returns an iterator over the list
     pub fn iter(self) -> PeekListLikeIter<'mem, 'facet_lifetime> {
+        let (as_ptr_fn, iter_vtable) = match self.def {
+            ListLikeDef::List(def) => (def.vtable.as_ptr, Some(def.vtable.iter_vtable)),
+            ListLikeDef::Array(def) => (Some(def.vtable.as_ptr), None),
+            ListLikeDef::Slice(def) => (Some(def.vtable.as_ptr), None),
+        };
+
+        let state = match (as_ptr_fn, iter_vtable) {
+            (Some(as_ptr_fn), _) => {
+                let data = unsafe { as_ptr_fn(self.value.data()) };
+                let layout = self
+                    .def
+                    .t()
+                    .layout
+                    .sized_layout()
+                    .expect("can only iterate over sized list elements");
+                let stride = layout.size();
+
+                PeekListLikeIterState::Ptr { data, stride }
+            }
+            (None, Some(vtable)) => {
+                let iter = unsafe { (vtable.init_with_value.unwrap())(self.value.data()) };
+                PeekListLikeIterState::Iter { iter, vtable }
+            }
+            (None, None) => unreachable!(),
+        };
+
         PeekListLikeIter {
-            list: self,
+            state,
             index: 0,
             len: self.len(),
+            def: self.def(),
+            _list: PhantomData,
         }
     }
 
