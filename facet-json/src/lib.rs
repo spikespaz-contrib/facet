1445a90b127d568e2171b1031b4d9ee4a428ad56
Amos Wenger <amos@bearcove.eu>
2025-05-30 22:57:25 +0200

    Tests pass again

--- facet-json/src/lib.rs
+++ facet-json/src/lib.rs
@@ -29,65 +29,65 @@ struct Json;
 #[cfg(feature = "std")]
 #[inline]
 fn write_json_string<W: Write>(writer: &mut W, s: &str) -> io::Result<()> {
-    // Just a little bit of text on how it works. There are two main steps:
-    // 1. Check if the string is completely ASCII and doesn't contain any quotes or backslashes or
-    //    control characters. This is the fast path, because it means that the bytes can be written
-    //    as they are, without any escaping needed. In this case we go over the string in windows
-    //    of 16 bytes (which is completely arbitrary, maybe find some real world data to tune this
-    //    with? I don't know and you don't have to do this dear reader.) and we just feed them into
-    //    the writer.
-    // 2. If the string is not completely ASCII or contains quotes or backslashes or control
-    //    characters, we need to escape them. This is the slow path, because it means that we need
-    //    to write the bytes one by one, and we need to figure out where to put the escapes. So we
-    //    just call `write_json_escaped_char` for each character.
+    // // Just a little bit of text on how it works. There are two main steps:
+    // // 1. Check if the string is completely ASCII and doesn't contain any quotes or backslashes or
+    // //    control characters. This is the fast path, because it means that the bytes can be written
+    // //    as they are, without any escaping needed. In this case we go over the string in windows
+    // //    of 16 bytes (which is completely arbitrary, maybe find some real world data to tune this
+    // //    with? I don't know and you don't have to do this dear reader.) and we just feed them into
+    // //    the writer.
+    // // 2. If the string is not completely ASCII or contains quotes or backslashes or control
+    // //    characters, we need to escape them. This is the slow path, because it means that we need
+    // //    to write the bytes one by one, and we need to figure out where to put the escapes. So we
+    // //    just call `write_json_escaped_char` for each character.
 
-    const STEP_SIZE: usize = 16;
-    type Window = u128;
-    type Chunk = [u8; STEP_SIZE];
+    // const STEP_SIZE: usize = 16;
+    // type Window = u128;
+    // type Chunk = [u8; STEP_SIZE];
 
     writer.write_all(b"\"")?;
 
     let mut idx = 0;
-    while idx + STEP_SIZE < s.len() {
-        let slice = &s[idx..idx + STEP_SIZE];
-        // Unwrap here is fine because the chunk is guaranteed to be exactly `CHUNK_SIZE` bytes long
-        // by construction.
-        let chunk = Chunk::try_from(slice.as_bytes()).unwrap();
-        let window = Window::from_ne_bytes(chunk);
-        // Our window is a concatenation of u8 values. For each value, we need to make sure that:
-        // 1. It is ASCII (i.e. the first bit of the u8 is 0, so u8 & 0x80 == 0)
-        // 2. It does not contain quotes (i.e. u8 & 0x22 != 0)
-        // 3. It does not contain backslashes (i.e. u8 & 0x5c != 0)
-        // 4. It does not contain control characters (i.e. characters below 32, including 0)
-        //    This means the bit above the 1st, 2nd or 3rd bit must be set, so u8 & 0xe0 != 0
-        let completely_ascii = window & 0x80808080808080808080808080808080 == 0;
-        let quote_free = !contains_0x22(window);
-        let backslash_free = !contains_0x5c(window);
-        let control_char_free = window & 0xe0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0 != 0;
-        if completely_ascii && quote_free && backslash_free && control_char_free {
-            // Yay! Whack it into the writer!
-            writer.write_all(slice.as_bytes())?;
-            idx += STEP_SIZE;
-        } else {
-            // Ahw one of the conditions not met. Let's take our time and artisanally handle each
-            // character.
-            let mut chars = s[idx..].chars();
-            for c in (&mut chars).take(STEP_SIZE) {
-                write_json_escaped_char(writer, c)?;
-            }
-            let bytes_consumed = chars.as_str().as_ptr() as usize - s[idx..].as_ptr() as usize;
-            idx += bytes_consumed;
-        }
-    }
+    // while idx + STEP_SIZE < s.len() {
+    //     let slice = &s[idx..idx + STEP_SIZE];
+    //     // Unwrap here is fine because the chunk is guaranteed to be exactly `CHUNK_SIZE` bytes long
+    //     // by construction.
+    //     let chunk = Chunk::try_from(slice.as_bytes()).unwrap();
+    //     let window = Window::from_ne_bytes(chunk);
+    //     // Our window is a concatenation of u8 values. For each value, we need to make sure that:
+    //     // 1. It is ASCII (i.e. the first bit of the u8 is 0, so u8 & 0x80 == 0)
+    //     // 2. It does not contain quotes (i.e. u8 & 0x22 != 0)
+    //     // 3. It does not contain backslashes (i.e. u8 & 0x5c != 0)
+    //     // 4. It does not contain control characters (i.e. characters below 32, including 0)
+    //     //    This means the bit above the 1st, 2nd or 3rd bit must be set, so u8 & 0xe0 != 0
+    //     let completely_ascii = window & 0x80808080808080808080808080808080 == 0;
+    //     let quote_free = !contains_0x22(window);
+    //     let backslash_free = !contains_0x5c(window);
+    //     let control_char_free = window & 0xe0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0 != 0;
+    //     if completely_ascii && quote_free && backslash_free && control_char_free {
+    //         // Yay! Whack it into the writer!
+    //         writer.write_all(slice.as_bytes())?;
+    //         idx += STEP_SIZE;
+    //     } else {
+    //         // Ahw one of the conditions not met. Let's take our time and artisanally handle each
+    //         // character.
+    //         let mut chars = s[idx..].chars();
+    //         for c in (&mut chars).take(STEP_SIZE) {
+    //             write_json_escaped_char(writer, c)?;
+    //         }
+    //         let bytes_consumed = chars.as_str().as_ptr() as usize - s[idx..].as_ptr() as usize;
+    //         idx += bytes_consumed;
+    //     }
+    // }
 
-    // In our loop we checked that we were able to consume at least `STEP_SIZE` bytes every
-    // iteration. That means there might be a small remnant at the end that we can handle in the
-    // slow method.
-    if idx < s.len() {
-        for c in s[idx..].chars() {
-            write_json_escaped_char(writer, c)?;
-        }
+    // // In our loop we checked that we were able to consume at least `STEP_SIZE` bytes every
+    // // iteration. That means there might be a small remnant at the end that we can handle in the
+    // // slow method.
+    // if idx < s.len() {
+    for c in s[idx..].chars() {
+        write_json_escaped_char(writer, c)?;
     }
+    // }
 
     writer.write_all(b"\"")
 }
