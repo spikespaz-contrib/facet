4b6ffc39dfec6fec805ea7da1fd83528c589b6aa
Luuk Wester <luuk.wester@gmail.com>
2025-05-30 12:09:55 +0200

    maybe make write_json_escaped_char faster

--- facet-json/src/lib.rs
+++ facet-json/src/lib.rs
@@ -57,6 +57,10 @@ fn write_json_escaped_char<W: Write>(writer: &mut W, c: char) -> io::Result<()> 
             buf[5] = s.as_bytes()[3];
             writer.write_all(&buf)
         }
+        c if c.is_ascii() => {
+            writer.write(&[c as u8])?;
+            Ok(())
+        }
         c => {
             let mut buf = [0; 4];
             let len = c.encode_utf8(&mut buf).len();
