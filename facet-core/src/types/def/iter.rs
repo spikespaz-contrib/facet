29323889884a1839a1cd5d87a6abd0b3d909f2e2
zyc <18611145971@163.com>
2025-06-01 22:45:50 +0800

    Fix

--- facet-core/src/types/def/iter.rs
+++ facet-core/src/types/def/iter.rs
@@ -117,11 +117,9 @@ impl<T: IterItem> IterVTableBuilder<T> {
     ///
     /// # Panics
     ///
-    /// This method will panic if any of the required fields are `None`.
+    /// Panic if any of the required fields (init_with_value, next, dealloc) are `None`.
     pub const fn build(self) -> IterVTable<T> {
         assert!(self.init_with_value.is_some());
-        assert!(self.next_back.is_some());
-        assert!(self.size_hint.is_some());
         IterVTable {
             init_with_value: self.init_with_value,
             next: self.next.unwrap(),
