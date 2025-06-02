4b59a4e6ed994a7a0662e79a5c83fe8d8f0b18a9
Amos Wenger <amos@bearcove.eu>
2025-06-02 20:44:41 +0200

    ...don't forget to uncomment code

--- outside-workspace/bigapi-indirection-facet/src/lib.rs
+++ outside-workspace/bigapi-indirection-facet/src/lib.rs
@@ -1,16 +1,16 @@
-// use bigapi_types_facet::generate_mock_catalog;
+use bigapi_types_facet::generate_mock_catalog;
 use facet_pretty::FacetPretty;
 
 pub fn do_ser_stuff() {
-    // // Generate a mock catalog
-    // let catalog = generate_mock_catalog();
+    // Generate a mock catalog
+    let catalog = generate_mock_catalog();
 
-    // // Serialize the catalog to JSON
-    // let serialized = facet_json::to_string(&catalog);
+    // Serialize the catalog to JSON
+    let serialized = facet_json::to_string(&catalog);
 
     let serialized = std::fs::read_to_string("/tmp/blah.json").unwrap();
 
-    // println!("Serialized catalog JSON.\n{}", serialized);
+    println!("Serialized catalog JSON.\n{}", serialized);
 
     // Deserialize back to a Catalog struct
     let deserialized: bigapi_types_facet::Catalog =
